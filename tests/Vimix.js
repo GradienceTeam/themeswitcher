e800d05a4337eb97f757bf6d0cb0be990209042c
Romain Vigier <romain@romainvigier.fr>
2020-02-14 01:27:24 +0100

    Fix typo

--- tests/Vimix.js
+++ tests/Vimix.js
@@ -4,22 +4,21 @@ const { Variants } = require('./_variants');
 
 ['', '-laptop'].forEach(size => {
 	['', '-beryl', '-doder', '-ruby'].forEach(color => {
-		// TODO: Fix laptop implementation
-		test.skip(`Vimix${size}${color}`, t => {
-			const variants = Variants.guess_from(`Vimix${size}${color}`);
-			t.is(variants.day, `Vimix${size}${color}`);
-			t.is(variants.night, `Vimix-dark${size}${color}`);
+		test(`vimix${size}${color}`, t => {
+			const variants = Variants.guess_from(`vimix${size}${color}`);
+			t.is(variants.day, `vimix${size}${color}`);
+			t.is(variants.night, `vimix-dark${size}${color}`);
 		});
 
-		test.skip(`Vimix-dark${size}${color}`, t => {
-			const variants = Variants.guess_from(`Vimix-dark${size}${color}`);
-			t.is(variants.day, `Vimix${size}${color}`);
-			t.is(variants.night, `Vimix-dark${size}${color}`);
+		test(`vimix-dark${size}${color}`, t => {
+			const variants = Variants.guess_from(`vimix-dark${size}${color}`);
+			t.is(variants.day, `vimix${size}${color}`);
+			t.is(variants.night, `vimix-dark${size}${color}`);
 		});
-		test.skip(`Vimix-light${size}${color}`, t => {
-			const variants = Variants.guess_from(`Vimix-light${size}${color}`);
-			t.is(variants.day, `Vimix-light${size}${color}`);
-			t.is(variants.night, `Vimix-dark${size}${color}`);
+		test(`vimix-light${size}${color}`, t => {
+			const variants = Variants.guess_from(`vimix-light${size}${color}`);
+			t.is(variants.day, `vimix-light${size}${color}`);
+			t.is(variants.night, `vimix-dark${size}${color}`);
 		});
 	});
 });
